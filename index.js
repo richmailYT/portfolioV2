60f3dfa6b6e55221e4e1c7884193ef66f0b42103
richmail <thisisforjunk56@gmail.com>
2023-10-18 12:44:02 -0400

    inital

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        
+    },
+    mounted() {
+        //this.init();
+    }
+})
+app.mount("#app");

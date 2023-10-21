4b1aeaeeb2c9cb570f12bfd0e0fc70b6c7b74027
richmail <thisisforjunk56@gmail.com>
2023-10-21 10:16:35 -0400

    added skills on hover effect

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const app = Vue.createApp({
     data() {
+        const skills = { nodejs: { slide_1: false, slide_2: false }, socketio: { slide_1: false, slide_2: false } }
         return {
-            
+            skills
         }
     },
     methods: {
-        
+        init() {
+            
+        },
+
+        change_active_skill(skill, bool) {
+            if (this.skills[skill].slide_1 == bool) {
+                return;
+            }
+
+             if (bool == true) {
+                this.skills[skill].slide_1 = bool;
+            } else {
+                this.skills[skill].slide_2 = bool;
+            }
+
+            setTimeout(() => {
+                if (bool == false) {
+                    this.skills[skill].slide_1 = bool;
+                } else {
+                    this.skills[skill].slide_2 = bool;
+                }
+            }, 200)
+        }
     },
     mounted() {
-        //this.init();
+        this.init();
     }
 })
 app.mount("#app");

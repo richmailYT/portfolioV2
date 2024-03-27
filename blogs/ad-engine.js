42d2829747b09b883783189ebede02d1dd735374
richmailYT <chasezou09@gmail.com>
2024-03-27 10:01:16 -0400

    First blog post whooo!

---
+++ blogs/ad-engine.js
@@ -0,0 +1,7 @@
+//https://www.detectadblock.com/
+console.log("he")
+const e = document.createElement("div");
+e.id = 'LAxQeZbEmqis';
+e.style.display = 'none';
+const app = document.getElementById("app");
+app.appendChild(e);
---
+++ blogs/global.js
@@ -0,0 +1,8 @@
+//https://www.detectadblock.com/
+
+//I need to do this this way so it can show the "disable adblock" message if someone has javascript disabled
+if (document.getElementById('LAxQeZbEmqis')) {
+    document.getElementById("FERufyQdmoXD").style.display = "none";
+} else {
+    document.getElementById("FERufyQdmoXD").style.display = "block";
+}
